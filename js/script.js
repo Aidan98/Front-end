004249814d72fd6002d439037c5192fd66dfef11
Aidan98 <aidanovic22@outlook.com>
2020-01-20 01:16:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-// const searchBtn = document.querySelector(".search");
-
-// function show (){
-
-// }
-
-// searchBtn.addEventlistener(click, show);
-
 let slider = document.getElementById("myRange");
 let output = document.getElementById("ammount");
+let bookmark = document.querySelector(".bookmarkIcon");
+
 output.textContent = slider.value
 // output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
-} 
+}
+
+
+let bookmarked = () => {
+  //check if bookmarked class has already been added
+  if (bookmark.classList.contains('bookmarked')) {
+    //remove bookmarked class
+    bookmark.classList.remove('bookmarked');
+  } else {
+    //otherwise add bookmarked class
+    bookmark.classList.add('bookmarked');
+  }
+}
+//if button is clicked trigger event
+bookmark.addEventListener('click', bookmarked);
